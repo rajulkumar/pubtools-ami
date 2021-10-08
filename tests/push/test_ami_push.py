89d3910c70d969913ae456af05663a6d2c8a45a9
rajulkumar <rajkumar@redhat.com>
2021-10-07 21:33:48 -0400

    updated test_push_ami

--- tests/push/test_ami_push.py
+++ tests/push/test_ami_push.py
@@ -1,9 +1,30 @@
 import os
 import re
+import shutil
 import pytest
 import logging
+import json
 from mock import patch, MagicMock
 from pubtools._ami.tasks.push import AmiPush, entry_point
+
+
+@pytest.fixture()
+def stage_ami():
+    path = "/tmp/aws_staged/region-1-hourly/AWS_IMAGES"
+    if os.path.exists("/tmp/aws_staged"):
+        shutil.rmtree("/tmp/aws_staged")
+    os.makedirs("/tmp/aws_staged/region-1-hourly/AWS_IMAGES", mode=0755)
+    open(os.path.join(path,"ami-1.raw"), 'a').close()
+
+    j_file = os.path.join(os.path.dirname(__file__), "data/aws_staged/pub-mapfile.json")
+    with open(j_file, 'r') as in_file:
+        with open("/tmp/aws_staged/pub-mapfile.json", 'w') as out_file:
+            data = json.load(in_file)
+            json.dump(data, out_file)
+    yield
+
+    if os.path.exists("/tmp/aws_staged"):
+        shutil.rmtree("/tmp/aws_staged")
 
 
 @pytest.fixture(autouse=True)
@@ -18,7 +39,7 @@ class FakeAmiPush(AmiPush):
 """     
 
 
-def test_do_push(command_tester, requests_mocker, mock_aws_publish, caplog):
+def test_do_push(command_tester, requests_mocker, mock_aws_publish, stage_ami):
     requests_mocker.register_uri('GET', re.compile("amazon/provider_image_groups"),
     json={"body":[{"name": "RHEL_HOURLY", "providerShortName": "awstest"},]})
     requests_mocker.register_uri('POST', re.compile("amazon/region"))
@@ -27,8 +48,8 @@ def test_do_push(command_tester, requests_mocker, mock_aws_publish, caplog):
     publish_rv = MagicMock(id='ami-1234567')
     publish_rv.name = "ami-rhel"
     mock_aws_publish.return_value = publish_rv
-    aws_staged = os.path.join(os.path.dirname(__file__), "data/aws_staged")
-
+    #aws_staged = os.path.join(os.path.dirname(__file__), "data/aws_staged")
+    aws_staged = "/tmp/aws_staged"
 
     command_tester.test(
         lambda: entry_point(AmiPush),
